17cc3a13b3df7f68c4b905accf061168dcfb9240
kwoklindsey <kwoklindsey0@gmail.com>
2023-07-13 13:03:40 -0700

    Initial Commit

---
+++ aboutMeApp/aboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeAppApp.swift
+//  aboutMeApp
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
