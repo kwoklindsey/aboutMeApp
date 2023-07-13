bfe739492bd2c86867504aca2ea4334bca03803e
kwoklindsey <kwoklindsey0@gmail.com>
2023-07-13 16:07:13 -0700

    Replaced the picture

--- aboutMeApp/ContentView.swift
+++ aboutMeApp/ContentView.swift
@@ -8,19 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var fact1 = ""
+    @State private var fact2 = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+        Color(.systemGreen)
+            .ignoresSafeArea()
+      VStack{
+        HStack {
+            Spacer()
+            Image("flowerfeild")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+                .frame(height : 300)
+
+            VStack{
+                Text("Lindsey Kwok")
+                    .font(.title)
+                Text("I ❤️ Kode with Klossy so soooo much ")
+                    .multilineTextAlignment(.center)
+                Button("Click for Facts") {
+                    fact1 = "nbfkldasjfkld"
+                    fact2 = "dfjadshfjkdshajfkkjdash"
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.blue)
+                Text(fact1)
+                    .multilineTextAlignment(.center)
+                Text(fact2)
+                    .multilineTextAlignment(.center)
+
+                
+            }
+            Spacer()
         }
-        .padding()
+          
+      }}
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
